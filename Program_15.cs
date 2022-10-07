c89601099e7c1f35bb200832686a8a8edf034126
SvetonosovS <tris23@bk.ru>
2022-10-07 16:00:55 +0300

    prog15

---
+++ Program_15.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int num;
+
+{
+while (true) 
+    {
+    Console.Write("Введите целое число: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+        break;
+    Console.WriteLine($"Ошибка ввода");   
+    }  
+
+Console.Write(num +" -> ");
+
+if (num >= 6 && num <= 7)
+    Console.WriteLine("Да");
+else if (num >= 1 && num <= 5)
+    Console.WriteLine("нет");
+else Console.WriteLine("Ошибка ввода");
+}
